caed67e2a8ca6ecfa240e60735424c67221059bf
Alex Hall <alex.mojaki@gmail.com>
2018-05-19 19:06:37 +0200

    Finish migrating tests

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
+import sqlite3
 import unittest
+from io import StringIO
+
+from littleutils import SimpleNamespace
 
 from sorcery import spells
-from sorcery.spells import unpack_keys, unpack_attrs, print_args, magic_kwargs
-from littleutils import SimpleNamespace
-from io import StringIO
+from sorcery.spells import unpack_keys, unpack_attrs, print_args, magic_kwargs, maybe
 
 
 class MyListWrapper(object):
@@ -121,6 +123,28 @@ x -
         y = 2
         self.assertEqual(foo.bar(x, y, z=3),
                          {('x', x), ('y', y), ('z', 3), foo})
+
+    def test_maybe(self):
+        n = None
+        assert maybe(n) is None
+        assert maybe(n).a.b.c()[4]().asd.asd()() is None
+        assert maybe(0) is 0
+        assert maybe({'a': 3})['a'] is 3
+        assert maybe({'a': {'b': 3}})['a']['b'] is 3
+        assert maybe({'a': {'b': 3}})['a']['b'] + 2 == 5
+        assert maybe({'a': {'b': None}})['a']['b'] is None
+
+    def test_select_from(self):
+        conn = sqlite3.connect(':memory:')
+        c = conn.cursor()
+        c.execute('CREATE TABLE points (x INT, y INT)')
+        c.execute("INSERT INTO points VALUES (5, 3), (8, 1)")
+        conn.commit()
+
+        assert [(3, 5), (1, 8)] == [(y, x) for y, x in spells.select_from('points')]
+        y = 1
+        x = spells.select_from('points', where=[y])
+        assert (x, y) == (8, 1)
 
 
 if __name__ == '__main__':
