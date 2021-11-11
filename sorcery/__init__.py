e3c3f31083a0f8aba9859e49a84c013a2f98b8d1
Alex Hall <alex.mojaki@gmail.com>
2021-11-11 20:48:55 +0200

    New packaging, switch to github actions

--- sorcery/__init__.py
+++ sorcery/__init__.py
@@ -3,4 +3,8 @@ from .core import spell, no_spells
 from .spells import unpack_keys, unpack_attrs, args_with_source, dict_of, print_args, call_with_name, delegate_to_attr, \
     maybe, select_from, magic_kwargs, assigned_names, switch, timeit
 
-__version__ = '0.2.1'
+try:
+    from .version import __version__
+except ImportError:  # pragma: no cover
+    # version.py is auto-generated with the git tag when building
+    __version__ = "???"
